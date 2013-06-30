177b6dde1970d61fc82275abad4f4399e3ff6e28
Audrey Penven <audrey.penven@gmail.com>
2013-06-29 22:30:47 -0700

    added some routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 FlickrClone::Application.routes.draw do
-  resources :photos
-  resources :users
-  resource :session
-  resources :groups
+  resources :groups do
+    resources :photos, :only [:index]
+    resources :photo_shares, only: [:create, :destroy]
+    resource :membership, only: [:create, :destroy]
+  end
+  resources :photos, only: [:create, :destroy, :show, :update] do
+    resources :photo_shares, only: [:create, :destroy]
+  end
+  resources :users do
+    resource :friendship, only: [:create, :destroy]
+  end
+  resource :session, only: [:create, :destroy, :new]
 end
