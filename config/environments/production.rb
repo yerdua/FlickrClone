a6fe10fd999a1e5e10712d45fd7b5a67b0ea7ba2
Audrey Penven <audrey.penven@gmail.com>
2013-07-02 17:52:53 -0700

    someday this shit will work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,8 +67,7 @@ FlickrClone::Application.configure do
   
   config.paperclip_defaults = {
     :storage => :s3,
-    :region => 'us-west-2',
-    :endpoint => 's3-us-west-2.amazonaws.com',
+    :s3_host_name => 's3-us-west-2.amazonaws.com',
     :s3_credentials => {
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
