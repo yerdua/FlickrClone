087089f172cf107dd9927a837b46681a2cf8fa4a
Audrey Penven <audrey.penven@gmail.com>
2013-07-19 18:30:44 -0700

    removed backbone. it wasn't being used

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,12 +14,4 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require underscore
-//= require backbone
-//= require backbone-relational
 //= require flickr_clone
-//= require_tree ../templates
-//= require_tree ./models
-//= require_tree ./collections
-//= require_tree ./views
-//= require_tree ./routers
-//= require_tree .
